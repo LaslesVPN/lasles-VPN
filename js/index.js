d26abe35e9d595b3379cf79662dff5d5dbfb0eaf
Umar-Juraev <77355492+Umar-Juraev@users.noreply.github.com>
2021-06-02 12:55:24 +0500

    deleted js falder

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const modal = $.modal({
-  title: "umar modal",
-  closable: true,
-  content: `
-  <p>can you show me top it one the top  of</p>
-  <p>can you show me top it one the top  of</p>
-  `,
-  width: "400px",
-});
